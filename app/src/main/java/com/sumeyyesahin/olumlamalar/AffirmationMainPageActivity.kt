729e63861781d2d720dd5cdd7c485bac7c78033a
Sümeyye Şahin <smyysahin823@gmail.com>
2024-04-30 22:29:46 +0300

    activities added

---
+++ app/src/main/java/com/sumeyyesahin/olumlamalar/AffirmationMainPageActivity.kt
@@ -0,0 +1,11 @@
+package com.sumeyyesahin.olumlamalar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AffirmationMainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_affirmation_main_page)
+    }
+}
---
+++ app/src/main/java/com/sumeyyesahin/olumlamalar/FavoriesActivity.kt
@@ -0,0 +1,11 @@
+package com.sumeyyesahin.olumlamalar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favories)
+    }
+}
