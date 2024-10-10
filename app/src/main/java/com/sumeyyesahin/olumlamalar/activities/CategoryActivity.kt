f85e4debc9a9ac7053cb3af7006c90c1f0ad982f
Sümeyye Şahin <smyysahin823@gmail.com>
2024-10-10 12:31:45 +0300

    CategoryViewModel and CategoryActivity Updated

--- app/src/main/java/com/sumeyyesahin/olumlamalar/activities/CategoryActivity.kt
+++ app/src/main/java/com/sumeyyesahin/olumlamalar/activities/CategoryActivity.kt
@@ -3,15 +3,21 @@ package com.sumeyyesahin.olumlamalar.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.sumeyyesahin.olumlamalar.helpers.DBHelper
 import com.sumeyyesahin.olumlamalar.adapters.CategoryAdapter
 import com.sumeyyesahin.olumlamalar.databinding.ActivityCategoryBinding
 import com.sumeyyesahin.olumlamalar.utils.GetSetUserLanguage.getUserLanguage
+import com.sumeyyesahin.olumlamalar.viewmodel.CategoryViewModel
 import java.util.Locale
+import androidx.activity.viewModels
 
 class CategoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCategoryBinding
+    private  val categoryViewModel: CategoryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +28,18 @@ class CategoryActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = ""
 
-        val language = intent.getStringExtra("language") ?: getUserLanguage(this)
-        setLocale(language)
-        val db = DBHelper(this)
 
-        val kategoriListesi = db.getAllCategoriesByLanguage(language)
-        val adapter = CategoryAdapter(kategoriListesi, language) // language parametresini geçiriyoruz
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.setHasFixedSize(true)
-        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+        val language = intent.getStringExtra("language") ?: getUserLanguage(this)
+        categoryViewModel.initialize(language)
+
+
+
+        categoryViewModel.getCategories(language).observe(this, Observer { categories ->
+            val adapter = CategoryAdapter(categories, language)
+            binding.recyclerView.adapter = adapter
+            binding.recyclerView.setHasFixedSize(true)
+            binding.recyclerView.layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+        })
     }
     override fun onBackPressed() {
         val intent = Intent(this, MainActivity::class.java)
@@ -39,20 +48,10 @@ class CategoryActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun setLocale(languageCode: String) {
-        val locale = Locale(languageCode)
-        Locale.setDefault(locale)
-        val config = resources.configuration
-        config.setLocale(locale)
-        resources.updateConfiguration(config, resources.displayMetrics)
-    }
 
     override fun onSupportNavigateUp(): Boolean {
 
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
-        finish()
+        onBackPressed()
         return true
     }
 }
--- app/src/main/java/com/sumeyyesahin/olumlamalar/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/sumeyyesahin/olumlamalar/viewmodel/CategoryViewModel.kt
@@ -1,9 +1,39 @@
 package com.sumeyyesahin.olumlamalar.viewmodel
 
 import android.app.Application
+import android.content.res.Configuration
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.sumeyyesahin.olumlamalar.adapters.CategoryAdapter
+import com.sumeyyesahin.olumlamalar.helpers.DBHelper
+import com.sumeyyesahin.olumlamalar.helpers.LocaleHelper.setLocale
+import com.sumeyyesahin.olumlamalar.utils.GetSetUserLanguage
+import java.util.Locale
 
 
 class CategoryViewModel(application : Application): AndroidViewModel(application) {
+
+    private val categoryList = MutableLiveData<List<String>>()
+
+    private val dbHelper = DBHelper(getApplication())
+
+    fun initialize(language: String) {
+        setLocale(language)
+    }
+
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration(getApplication<Application>().resources.configuration)
+        config.setLocale(locale)
+        getApplication<Application>().resources.updateConfiguration(config, getApplication<Application>().resources.displayMetrics)
+    }
+
+    fun getCategories(language: String): MutableLiveData<List<String>> {
+        val kategoriListesi = dbHelper.getAllCategoriesByLanguage(language)
+        categoryList.value = kategoriListesi
+        return categoryList
+    }
+
 
 }
